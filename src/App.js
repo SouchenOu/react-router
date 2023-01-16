65d9f1d941ef9fc636d950bedc08afbadd0d0b24
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-01-16 10:08:32 +0100

    add new element to the navbar

--- src/App.js
+++ src/App.js
@@ -14,6 +14,15 @@ function App() {
       <li>
         <Link to="/books">Books</Link>
       </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+      <li>
+        <Link to="/newBook">newBook</Link>
+      </li>
+      <li>
+        <Link to="/About">About</Link>
+      </li>
     </ul>
     </nav>
   <Routes>
@@ -35,4 +44,10 @@ export default App;
 // so how we can use react router
 //1: you need first to import your router into your index.js page
 //2:you need to define all your routes inside of this routes component
-//3:and you need to use different link tags to move around your application to all of these diferent routes
+//3:and you need to use different link tags to move around your application to all of these diferent routes\
+
+
+/******there is different type of browser you can use the browser router is not only the router that is available there is another router called Hashrouter */
+
+// Hashrouter is very similar to the browser-router in that it works inside your browser but instead of storing the url as a normal url it stores inside a hashtag portion of our url
+//and there is also (unstable_historyRouter) and the way this history router works is allows you to actually take control completely of the history element in react router so the history element just determines how your entire browser renders out the history of all the pages you have been on so 
