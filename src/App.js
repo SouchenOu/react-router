da1d2f87cb2cb690ce917f4cfcb8572d8ce5f161
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-01-16 10:27:16 +0100

    check all the browser router

--- src/App.js
+++ src/App.js
@@ -49,5 +49,8 @@ export default App;
 
 /******there is different type of browser you can use the browser router is not only the router that is available there is another router called Hashrouter */
 
-// Hashrouter is very similar to the browser-router in that it works inside your browser but instead of storing the url as a normal url it stores inside a hashtag portion of our url
-//and there is also (unstable_historyRouter) and the way this history router works is allows you to actually take control completely of the history element in react router so the history element just determines how your entire browser renders out the history of all the pages you have been on so 
+// (Hashrouter**) is very similar to the (browser-router**) in that it works inside your browser but instead of storing the url as a normal url it stores inside a hashtag portion of our url (http://localhost:3000/#/About)
+//and there is also (unstable_historyRouter**) and the way this history router works is allows you to actually take control completely of the history element in react router so the history element just determines how your entire browser renders out the history of all the pages you have been on so for example when i click the back button it properly send me back and when i click the forward button it properly sends me forward that is considered like your browser history
+// and there is (MemoryRouter) it is different than all the other routers and that actually stores everything related to where you have been in history inside of memory and it doesnt rely on the url bar of our browser ( means that if i change the page the url doesnt change)
+//another one is (StaticRouter) [import {staticRouter} from "react-router-dom/server"]this router specificly for writing on the server this router is different to others it doesnt actually allow you to change pages it specifies a single url and you do that by specifying a location so we do just { <Staticrouter location= "/book"> } so after that the page doesnt change  
+// and there is (nativeRouter) we use it if we use ReactNative
