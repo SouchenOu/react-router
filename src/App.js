55d1d1a67d39fe4023fcbe0c105b0c5f5aed1969
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-01-16 10:31:11 +0100

    finish testing all router

--- src/App.js
+++ src/App.js
@@ -37,17 +37,19 @@ function App() {
 
 export default App;
 
-// we will going to use a compenent calling the routes this routes comes from (react-router-dom) and inside of this routes compenent we need to use another compenent calling
-//the route to define the routes compenent  
+/* we will going to use a component calling the routes this routes comes from (react-router-dom) and inside of this routes component we need to use another component calling
+the route to define the routes compenent  */
 // route here define the path to different page website
+
 //<Route path="/" element={ <Home/> } /> (here in index path we will going to home compenent)
-// so how we can use react router
+
+/*********** so how we can use react router******/
 //1: you need first to import your router into your index.js page
 //2:you need to define all your routes inside of this routes component
-//3:and you need to use different link tags to move around your application to all of these diferent routes\
+//3:and you need to use different link tags to move around your application to all of these different routes\
 
 
-/******there is different type of browser you can use the browser router is not only the router that is available there is another router called Hashrouter */
+/******there is different type of browser you can use the (browser router) is not only the router that is available there is another router called */
 
 // (Hashrouter**) is very similar to the (browser-router**) in that it works inside your browser but instead of storing the url as a normal url it stores inside a hashtag portion of our url (http://localhost:3000/#/About)
 //and there is also (unstable_historyRouter**) and the way this history router works is allows you to actually take control completely of the history element in react router so the history element just determines how your entire browser renders out the history of all the pages you have been on so for example when i click the back button it properly send me back and when i click the forward button it properly sends me forward that is considered like your browser history
